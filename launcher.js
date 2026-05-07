8f4a77968d74d14a93217d3783b824f27cca9244
pdubz3r0 <pdubz3r0@proton.me>
2026-05-07 04:01:27 -0400

    add launcher for node/bun selection based on what called package.json

---
+++ launcher.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { isPackageManager, isNpm, isYarn, isPnpm, isBun } from 'is-npm';
+import { spawnSync } from "node:child_process";
+import { fileURLToPath } from 'node:url';
+
+export function nodepackagemgmt(){
+	if (isBun) {
+		return "bun"
+	} else {
+		return "node"
+	}
+}
+
+/**
+    Determine if this script was called directly from the command line or imported to use the nodepackagemgmt method.
+ */
+const main = (()=>{ 
+  if (typeof import.meta.main === "undefined") {
+    const __filename = fileURLToPath(import.meta.url);
+    return (process.argv[1] === __filename)
+  } else {
+    return import.meta.main;
+  }
+})()
+
+if (main) {
+	const result = spawnSync(nodepackagemgmt(), process.argv.slice(2), { stdio: "inherit" });
+	process.exit(result.status ?? 1);
+}
