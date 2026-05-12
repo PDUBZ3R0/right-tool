94701f6aa1bdb770fa29d538f1038796d75d63c0
pdubz3r0 <pdubz3r0@proton.me>
2026-05-12 06:20:50 -0400

    forgot to save a file

--- cascade.js
+++ cascade.js
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
 
-import { isPackageManager, isNpm, isYarn, isPnpm, isBun } from 'is-npm';
 import { spawnSync } from "node:child_process";
 import { main } from './main.js'
 
-export function nodepackagemgmt(){
-	if (isBun) {
-		return "bun"
-	} else if (isYarn) {
-		return "yarn"
-	} else if (isPnpm) {
-		return "pnpm"
-	} else {
-		return "npm"
-	}
-}
+import { nodepackagemgmt } from './packagemgmt.js'
 
 if (main(import.meta.url)) {
 	const result = spawnSync(nodepackagemgmt(), process.argv.slice(2), { stdio: "inherit" });
